44004dc906fc4e7253ee2beb8e852bfc63c5f80d
Colin Knebl <colin.knebl@outlook.com>
2020-09-17 11:09:41 -0400

    fix width and height bug

--- src/GraphicsEditor/Shapes/Rectangle/Rectangle.ts
+++ src/GraphicsEditor/Shapes/Rectangle/Rectangle.ts
@@ -10,21 +10,20 @@ export class Rectangle extends Shape {
     protected _hover(): void {
         const size = Shape.outline.hover.size;
         this._ctx.fillStyle = Shape.outline.hover.color;
-        this._ctx.fillRect(this.x - size, this.y - size, this.height + size * 2, this.width + size * 2);
+        this._ctx.fillRect(this.x - size, this.y - size, this.width + size * 2, this.height + size * 2);
         this._draw();
     }
 
     protected _select(): void {
         const size = Shape.outline.hover.size;
-        this._ctx.strokeRect(this.x - size, this.y - size, this.height + size * 2, this.width + size * 2);
+        this._ctx.strokeRect(this.x - size, this.y - size, this.width + size * 2, this.height + size * 2);
     }
 
     protected _isPointOver(x: number, y: number, boundingRect: DOMRect) {
-        const canvasPosition = boundingRect;
-        const shapeLeftEdge = this.x + canvasPosition.left;
-        const shapeRightEdge = this.x + canvasPosition.left + this.width;
-        const shapeTopEdge = this.y + canvasPosition.top;
-        const shapeBottomEdge = this.y + canvasPosition.top + this.height;
+        const shapeLeftEdge = this.x + boundingRect.left;
+        const shapeRightEdge = shapeLeftEdge + this.width;
+        const shapeTopEdge = this.y + boundingRect.top;
+        const shapeBottomEdge = shapeTopEdge + this.height;
 
         if (x > shapeLeftEdge && x < shapeRightEdge &&
             y > shapeTopEdge && y < shapeBottomEdge) {
@@ -35,7 +34,7 @@ export class Rectangle extends Shape {
 
     protected _draw(): Shape {
         this._ctx.fillStyle = this.color;
-        this._ctx.fillRect(this.x, this.y, this.height, this.width);
+        this._ctx.fillRect(this.x, this.y, this.width, this.height);
         // this._ctx.clearRect(45, 45, 60, 60);
         // this._ctx.strokeRect(50, 50, 50, 50);
         // this._ctx.save();
