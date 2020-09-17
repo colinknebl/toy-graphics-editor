90bafdc55dddf58c7876990dec12f714ccae971a
Colin Knebl <colin.knebl@outlook.com>
2020-09-17 17:40:13 -0400

    add code seperators to Rectangle

--- src/GraphicsEditor/Shapes/Rectangle/Rectangle.ts
+++ src/GraphicsEditor/Shapes/Rectangle/Rectangle.ts
@@ -3,10 +3,12 @@ import type { Canvas } from "../../Canvas/Canvas";
 
 export class Rectangle extends Shape {
     
+    // ========================================================================
     public constructor(CanvasType: typeof Canvas) {
         super(CanvasType, {height: 100, width: 100});
     }
 
+    // ========================================================================
     protected _hover(): void {
         const size = Shape.outline.hover.size;
         this._ctx.fillStyle = Shape.outline.hover.color;
@@ -14,11 +16,13 @@ export class Rectangle extends Shape {
         this._draw();
     }
 
+    // ========================================================================
     protected _select(): void {
         const size = Shape.outline.hover.size;
         this._ctx.strokeRect(this.x - size, this.y - size, this.width + size * 2, this.height + size * 2);
     }
 
+    // ========================================================================
     protected _isPointOver(x: number, y: number, boundingRect: DOMRect) {
         const shapeLeftEdge = this.x + boundingRect.left;
         const shapeRightEdge = shapeLeftEdge + this.width;
@@ -32,13 +36,10 @@ export class Rectangle extends Shape {
         return false;
     }
 
+    // ========================================================================
     protected _draw(): Shape {
         this._ctx.fillStyle = this.color;
         this._ctx.fillRect(this.x, this.y, this.width, this.height);
-        // this._ctx.clearRect(45, 45, 60, 60);
-        // this._ctx.strokeRect(50, 50, 50, 50);
-        // this._ctx.save();
-
         return this;
     }
 }
